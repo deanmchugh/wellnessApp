db663db1ccfcf413fa23c84c50a828fee234ae8d
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-25 17:50:05 +1000

    styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 export default function Header() {
     return (
-        <View style={{flexDirection: 'row', height: 50, borderWidth: 1, borderColor: 'red'}}>
-            <View style={{flex: 1, borderColor: 'red', borderWidth: 1}}></View>
-            <View style={{flex: 2, justifyContent: 'center', borderColor: 'red', borderWidth: 1}}>
-                <Text style={{textAlign: 'center'}}>Title</Text>
+        <View style={styles.container}>
+            <View style={styles.sectionWidthOne}></View>
+            <View style={styles.sectionTitle}>
+                <Text style={styles.title}>Title</Text>
             </View>
-            <View style={{flex: 1, borderColor: 'red', borderWidth: 1}}></View>
+            <View style={styles.sectionWidthOne}></View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row', 
+        height: 50, 
+        borderWidth: 1, 
+        borderColor: 'red'
+    },
+    sectionWidthOne: {
+        flex: 1,
+        borderColor: 'red',
+        borderWidth: 1
+    },
+    sectionTitle: {
+        flex: 2, 
+        justifyContent: 'center',
+        borderColor: 'red', 
+        borderWidth: 1
+    },
+    title: {
+        textAlign: 'center'
+    }
+})
